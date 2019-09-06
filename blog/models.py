bf73b8092de37ea1be5adbb6fd230f36fce8d057
Shrawan Chakradhar <bijutmg12@gmail.com>
2019-09-06 15:32:35 +0545

    This is the homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = models.DateField()
+    describ = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
